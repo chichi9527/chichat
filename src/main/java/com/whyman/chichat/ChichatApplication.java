9b389a24507fa2d20f57d958231798f02f66961b
A0216286@ulive.pccu.edu.tw <A0216286@ulive.pccu.edu.tw>
2022-02-01 10:57:53 +0800

    Initial commit

---
+++ src/main/java/com/whyman/chichat/ChichatApplication.java
@@ -0,0 +1,13 @@
+package com.whyman.chichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChichatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whyman/chichat/ChichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyman.chichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChichatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
